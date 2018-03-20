60e0d20cbfc0a93ac681a319968e69fc88c98e22
Svetoslav Velichkov <svetoslav.s.velichkov@gmail.com>
2018-03-20 16:38:28 +0200

    Add necessary setup for import through Carthage
    This change turns Alamofire-Synchronous into a proper project, which can
    then be imported through Carthage.
    
    Also, .gitignore was slightly updated to exclude the Carthage directory
    and some macOS extraneous files.

---
+++ Resources/Alamofire_Synchronous.h
@@ -0,0 +1,19 @@
+//
+//  Alamofire_Synchronous.h
+//  Alamofire_Synchronous
+//
+//  Created by Svetoslav on 20/3/18.
+//  Copyright © 2018 Svetoslav Velichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alamofire_Synchronous.
+FOUNDATION_EXPORT double Alamofire_SynchronousVersionNumber;
+
+//! Project version string for Alamofire_Synchronous.
+FOUNDATION_EXPORT const unsigned char Alamofire_SynchronousVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alamofire_Synchronous/PublicHeader.h>
+
+
